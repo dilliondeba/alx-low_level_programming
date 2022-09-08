37929bf8b80dc09032e7f1cc3a7aef542f8ea87d
Dillion Dalu <daluudeze@gmail.com>
2022-09-08 12:57:05 -0700

    thi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-/**
- *
+ #include <stdio.h>
+ /**
+ *main - entry point
  *
  *
  * Return: 0
@@ -8,6 +8,6 @@
  int main(void)
  {
 	 puts("\"Programming is like building a multilingual puzzle");
-		 return(0);
+		 return (0);
 
  }
