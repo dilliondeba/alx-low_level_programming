29c68739a91d39e86d58871b6930491c5e4a05c6
Dillion Dalu <daluudeze@gmail.com>
2022-09-10 00:53:02 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,47 @@
-#include <stdio.h>
-#include <time.h>
+#include<stdio.h>
+
 #include <stdlib.h>
 
- /**
-  * main - Entry point
-  *
-  * Description: print the value of n status
-  * greater than, is zero and is not less than 6.
-  *
-  * Return: Always 0 (Success)
-  */
+#include <time.h>
+
+/**
+ *
+ * *main - Entry point
+ *
+ * *Description: print the value of n status:
+ *
+ * *greater than, is zero and is not less than 6.
+ *
+ * *Return: Always O (Success)
+ *
+ * */
 
 int main(void)
-	/*dillion code*/
+
 {
-	int n;
-	int last;
+
+	int n, digit;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last = n % 10;
 
-	/*my code*/
-	if (last > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	else if (last == 0)
-		printf("last digit of %d is %d and is 0\n", n, last);
-	else if (last < 6 && last != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+
+	digit = n % 10; /*gets the digit value*/
+
+	if (digit > 5)
+
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+
+	else if (digit == 0)
+
+		printf("Last digit of %d is %d and is 0\n", n, digit);
+
+	else if (digit < 6 && digit != 0)
+
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 
 	return (0);
-}
+
+
