55f15c2d564ebdb68b52b171d17e735c58b997c3
Dillion Dalu <daluudeze@gmail.com>
2022-09-09 23:39:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters except q and e
+ *
+ * Return; Always 0 (Success)
+ */
+
+int main(void)
+{
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		if (al != 'q' && al != 'e')
+		{
+			putchar(al);
+		}
+		al++;
+	}
+	putchar('\n');
+
+	return (0);
+}
