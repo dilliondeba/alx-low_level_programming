15f5cc87fdeaf5d9ee03aecbd08b92b0b401ec0c
Dillion Dalu <daluudeze@gmail.com>
2022-09-09 23:02:24 -0700

    changes and commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowecase then in uppercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char al = 'a';
+	char AL = 'A';
+
+	while (al <= 'z')
+	{
+		putchar(al);
+		al++;
+	}
+
+	while (AL <= 'Z')
+	{
+		putchar(AL);
+		AL++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+		
