6dd6f54a83e2f1cb7856d9e4c38bce3514b57a68
Dillion Dalu <daluudeze@gmail.com>
2022-09-10 07:06:58 -0700

    to be commited

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,12 @@
 #include<stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
 /**
- *
- * *main - Entry point
- *
- * *Description: print the value of n status:
- *
- * *greater than, is zero and is not less than 6.
- *
- * *Return: Always O (Success)
- *
- * */
+ * main - Entry point
+ * Description: print the value of n status:
+ * greater than, is zero and is not less than 6.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
@@ -44,4 +36,4 @@ int main(void)
 
 	return (0);
 
-
+}
