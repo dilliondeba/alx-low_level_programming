7a1ca96b565d5e0aba0986ad2152c1e4401cd8f3
Dillion Dalu <daluudeze@gmail.com>
2022-09-15 13:46:03 -0700

    more funtions loop

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints except 2 and 4.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{ 
+	int n = 0;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+
+		n++;
+	}
+	_putchar('\n');
+}
