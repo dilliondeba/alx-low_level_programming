79f0581e823319eb06e0bcb559f3800aee4f6898
Dillion Dalu <daluudeze@gmail.com>
2022-09-27 13:29:26 -0700

    first batch commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
 /**
  * _memset - a function that fills memory with constant bytes
- * @s: location to fill
- * @b: char to fill loction with
- * @n: number of bytes to fill
- * Return: returns pointer to location filled
+ * @s:char
+ * @b:char
+ * @n:unsigned int
+ * Return:char
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *start = s;
+	unsigned int i;
 
-	while (n--)
-	{
-		*s = b;
-		s++;
-	}
-	return (start);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
 }
