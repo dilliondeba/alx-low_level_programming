763e8eccf58775237fba7e18db3767cb38a4ecac
Dillion Dalu <daluudeze@gmail.com>
2022-10-06 10:28:12 -0700

    last commits

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *mall_checked - allocates memory using malloc and exist if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *new_mem;
+
+	new_mem = malloc(b);
+	if (new_mem == NULL)
+		exit(98);
+	return (new_mem);
+}
