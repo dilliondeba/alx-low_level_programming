dc477c8377f0f8c3aaaf15474c6ad19f739b1629
Dillion Dalu <daluudeze@gmail.com>
2022-10-05 21:49:57 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,16 +8,16 @@
 
 char *_strdup(char *str)
 {
-	int i = 0, 1 = 0;
+	int i = 0, l = 0;
 	char *s;
 
 	if (str == NULL)
 		return (NULL);
 
-	while (str[1] != '\0')
-		1++;
+	while (str[l] != '\0')
+		l++;
 
-	s = malloc((1 + 1) * sizeof(char));
+	s = malloc((l + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 
