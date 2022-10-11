7c71f76425be847d2fc31cc9933e6fbb889c575f
Dillion Dalu <daluudeze@gmail.com>
2022-10-11 05:57:27 -0700

    task update

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * DEsc: Header file that defines a function-like macro SUM(x, y)
+ *	that computes the sum of the number x and y.
+ */
+
+#define Sum(x, y) ((x) + (y))
+
+#endif
